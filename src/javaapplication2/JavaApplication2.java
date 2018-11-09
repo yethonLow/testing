9c42a4763b5a5e1e1296c99e4214029c28fa2c0c
taruc <taruc@B25>
2018-11-09 10:39:59 +0800

    Test1

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("hello testing");
+        System.out.println("bye testing");
     }
     
 }
